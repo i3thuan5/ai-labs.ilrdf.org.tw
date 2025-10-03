dc962ca7df525584fd016f5bbf28a675356c3113
bunli <bunli@ithuan.tw>
2025-10-03 17:17:37 +0800

    * Tox.ini「test」改測試Django Testcase，並以此做紅燈測試。

--- autuan/bangtsam/admin.py
+++ autuan/bangtsam/admin.py
@@ -9,7 +9,7 @@ class AutaiSite(admin.AdminSite):
     site_header = "族語AI成果網站人員管理後台"
 
 
-autai_site = AutaiSite(name="adminautai")
+autai_site = AutaiSite(name="adminautaidddd")
 
 autai_site.register(User, UserAdmin)
 autai_site.register(Group)
--- autuan/bangtsam/tests.py
+++ autuan/bangtsam/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+def tshong_superuser():
+    superuser = User.objects.create_superuser(
+        'kuanliuan', 'kuan@ithuan.tw', 'ku#va0ti17@')
+    return superuser
+
+
+class AutaiTest(TestCase):
+    def test_autai_url(self):
+        self.client.force_login(tshong_superuser())
+        response = self.client.get(
+            reverse("adminautai:auth_user_changelist"), follow=True)
+        self.assertEqual(response.status_code, 200)

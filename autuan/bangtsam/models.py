6f826aad7a658ec2f5a77f87d0413192e9a5938c
bunli <bunli@ithuan.tw>
2025-10-09 11:06:25 +0800

    + HomePage, RichTextBasePage, SitemapPage, IntroPage, FeedbackPage, ApplicationFormPage, AboutUsPage, TermsOfUsePage

--- autuan/bangtsam/models.py
+++ autuan/bangtsam/models.py
@@ -1,2 +1,48 @@
+from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
-# Create your models here.
+
+class HomePage(Page):
+    parent_page_types = ['wagtailcore.Page']
+    max_count_per_parent = 1
+
+
+class RichTextBasePage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
+
+    parent_page_types = ['HomePage']
+    subpage_types = []
+    max_count_per_parent = 1
+
+    class Meta:
+        abstract = True
+
+
+class SitemapPage(RichTextBasePage):
+    pass
+
+
+class IntroPage(RichTextBasePage):
+    pass
+
+
+class FeedbackPage(RichTextBasePage):
+    pass
+
+
+class ApplicationFormPage(RichTextBasePage):
+    pass
+
+
+class AboutUsPage(RichTextBasePage):
+    pass
+
+
+class TermsOfUsePage(RichTextBasePage):
+    pass

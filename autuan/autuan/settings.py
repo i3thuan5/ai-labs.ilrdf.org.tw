c8b2c6013c05ae5e0fc289a796e4ca9becfd452e
bunli <bunli@ithuan.tw>
2025-10-12 09:32:14 +0800

    * 限制Wagtail後台圖檔上傳大小為3MB以下。

--- autuan/autuan/settings.py
+++ autuan/autuan/settings.py
@@ -171,5 +171,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 WAGTAIL_SITE_NAME = '成果網站內容後台'
 WAGTAILADMIN_BASE_URL = 'localhost'
 WAGTAILIMAGES_EXTENSIONS = ['svg', 'png', 'jpg', 'webp', ]
+WAGTAILIMAGES_MAX_UPLOAD_SIZE = 3 * 1024 * 1024
 WAGTAILDOCS_EXTENSIONS = ['docx', 'odt', 'pdf', 'txt', 'wav', ]
 WAGTAIL_PASSWORD_RESET_ENABLED = False  # 關閉「忘記密碼？」連結

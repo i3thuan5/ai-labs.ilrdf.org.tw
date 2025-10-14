8b84aa40234667ebf6ef9fec08750513c2c6aa2b
bunli <bunli@ithuan.tw>
2025-10-14 16:48:24 +0800

    * 綠燈測試：超過5MB的檔案會被擋下來。

--- autuan/bangtsam/forms/document_base.py
+++ autuan/bangtsam/forms/document_base.py
@@ -11,5 +11,5 @@ class CustomDocumentForm(BaseDocumentForm):
             and cleaned_data["file"].size > settings.SAPOLITA_DOCS_MAX_UPLOAD_SIZE
         ):
             max_size_mb = settings.SAPOLITA_DOCS_MAX_UPLOAD_SIZE / (1024*1024)
-            # self.add_error("file", f"文件檔案請小於{max_size_mb}MB以下。")
+            self.add_error("file", f"文件檔案請小於{max_size_mb}MB以下。")
         return cleaned_data

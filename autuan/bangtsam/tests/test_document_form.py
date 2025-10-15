10e3a2ace2ba1a1f72fdd091f9ce85b627cd6d90
bunli <bunli@ithuan.tw>
2025-10-15 09:43:16 +0800

    * 紅燈測試：CustomDocumentForm無收著file參數

--- autuan/bangtsam/tests/test_document_form.py
+++ autuan/bangtsam/tests/test_document_form.py
@@ -5,6 +5,7 @@ from wagtail.documents.forms import get_document_form
 
 
 class DocumentFormTest(TestCase):
+
     def test_small_document_upload_success(self):
         content_string = b'hello world'
         form_data = {
@@ -41,3 +42,20 @@ class DocumentFormTest(TestCase):
                 '文件檔案請小於5.0MB以下。',
                 '不允許副檔名為 “” 。可用的像是: docx, odt, pdf, txt, wav。'
             ])
+
+    def test_invalid_none_file_data(self):
+        content_string = b'hello world'
+        form_data = {
+            "title": "1KB Document.txt",
+            "tags": [],
+        }
+        file_data = {}
+        form_cls = get_document_form(models.Document)
+        form = form_cls(form_data, file_data)
+        # self.assertFormError(
+        #     form, 'file',
+        #     [
+        #         '必要欄位',
+        #         '不允許副檔名為 “” 。可用的像是: docx, odt, pdf, txt, wav。'
+        #     ])
+        self.assertTrue(form.is_valid(), msg=form.errors)

76e6db96b9b82d2aa53cea828d55166546843b91
bunli <bunli@ithuan.tw>
2025-10-15 16:26:47 +0800

    Revert "* 用DOMPurify消毒。"
    This reverts commit ec1f61985c22038ce72f8a4dde1220c62c46e3f9.

--- autuan/static/js/sapolita.js
+++ autuan/static/js/sapolita.js
@@ -4,22 +4,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 	for (let i=0;i<img_tin.length;i++){
 		let img = img_tin[i];
-		let btn = document.createElement('button');
-		btn.setAttribute('type', 'button');
-		btn.dataset.bsToggle = 'modal';
-		btn.dataset.bsTarget = '#myModal' + i;
-
-		img.before(btn);
-		btn.appendChild(img);
+		let wrapper = document.createElement('button');
+		wrapper.setAttribute('type', 'button');	
+		wrapper.dataset.bsToggle = 'modal';
+		wrapper.dataset.bsTarget = '#myModal' + i;
+		img.before(wrapper);
+		wrapper.appendChild(img);
 		createModelElement(i, img);
-
 	}
 
 	function createModelElement(mid, img){
 		const parser = new DOMParser();
 		const src = img.getAttribute('src');
 		const alt = img.getAttribute('alt');
-		let html = DOMPurify.sanitize(`
+		let html = `
+			<!-- Modal -->
 			<div class="modal fade" id="myModal${mid}" tabindex="-1" 
 				aria-labelledby="modalabel${mid}" aria-hidden="true">
 			  <div class="modal-dialog modal-dialog-centered">
@@ -39,7 +38,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 			      </div>
 			    </div>
 			  </div>
-			</div>`);
+			</div>`;
 
 		const doc = parser.parseFromString(html, "text/html");
 		const modalElement = doc.body.firstElementChild;
@@ -53,6 +52,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 		modalElement.addEventListener('shown.bs.modal', function (event) {
 			const upclosebtn = modalElement.getElementsByClassName('btn-close')[0];
 			if(upclosebtn){
+				console.log('upclosebtn', upclosebtn)
 				upclosebtn.focus();
 			}
 		});

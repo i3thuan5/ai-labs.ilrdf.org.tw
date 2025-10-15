657c16e7f8293509c7c0e78a09a02e75b191147b
bunli <bunli@ithuan.tw>
2025-10-15 10:07:26 +0800

    * 紅燈測試：檢查副檔名是用suffix而非用suffixes

--- autuan/bangtsam/blocks.py
+++ autuan/bangtsam/blocks.py
@@ -14,7 +14,7 @@ class SampleBlock(StructBlock):
     def clean(self, value):
         result = super().clean(value)
         audio = result["audio"]
-        file_type = ''.join(Path(audio.file.path).suffixes)
+        file_type = ''.join(Path(audio.file.path).suffix)
         if file_type != '.wav':
             raise StructBlockValidationError(block_errors={
                 "audio": ValidationError("音檔欄位限定上傳.wav格式")

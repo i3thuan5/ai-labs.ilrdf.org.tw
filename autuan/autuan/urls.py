a898d38818b87d64b175fe29283686ab7b1b538c
bunli <bunli@ithuan.tw>
2025-10-15 09:34:39 +0800

    * urlpatterns修正。

--- autuan/autuan/urls.py
+++ autuan/autuan/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -29,10 +28,15 @@ urlpatterns = [
     path('kuanli/', autai_site.urls),
     path('katayalan/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
-    path('pages/', include(wagtail_urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns = urlpatterns + [
-    path("", include(bangtsam_urls)),
-    path("", include(wagtail_urls)),
+    path('', include(wagtail_urls)),
+    path('', include(bangtsam_urls)),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

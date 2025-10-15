e5f587c1aab99ed7d3284f9684cf53ff7fcf0dcb
bunli <bunli@ithuan.tw>
2025-10-15 09:19:30 +0800

    * 紅燈測試：前台導覽列網址無提live版本的。

---
+++ autuan/bangtsam/tests/test_context_processors.py
@@ -0,0 +1,36 @@
+from wagtail.test.utils import WagtailPageTestCase
+
+from bangtsam.models import HomePage, IntroPage, SitemapPage
+from bangtsam.tests.utils import tshong_wagtail_homepage
+
+
+class ThehWagtailPagesTest(WagtailPageTestCase):
+
+    def test_theh_wagtail_pages_into_template(self):
+        homepage = tshong_wagtail_homepage()
+        response = self.client.get(homepage.url)
+        self.assertEqual(response.context['homepage'], homepage)
+        self.assertEqual(response.context['sitemappage'], None)
+        self.assertEqual(response.context['intropage'], None)
+        self.assertEqual(response.context['feedbackpage'], None)
+        self.assertEqual(response.context['applicationformpage'], None)
+        self.assertEqual(response.context['aboutuspage'], None)
+        self.assertEqual(response.context['termofusepage'], None)
+        self.assertEqual(response.context['copyrightpage'], None)
+
+
+def test_page_must_be_live(self):
+    homepage = tshong_wagtail_homepage()
+    homepage.add_child(
+        instance=IntroPage(
+            title='計畫介紹', slug='intro', live=False
+        )
+    )
+    homepage.add_child(
+        instance=SitemapPage(
+            title='網站地圖', slug='sitemap', live=True
+        )
+    )
+    response = self.client.get(homepage.url)
+    self.assertNotContains(response, '/introslug/')
+    self.assertContains(response, '/sitemap/')

ea8756f9ad6514985cdcd4c5406135b645232cee
bunli <bunli@ithuan.tw>
2025-10-30 18:13:47 +0800

    * 嘗試測試500。

--- autuan/bangtsam/tests/views/test_handler.py
+++ autuan/bangtsam/tests/views/test_handler.py
@@ -1,4 +1,7 @@
-from django.test import TestCase, override_settings
+from unittest.mock import patch, DEFAULT
+from django.test import TestCase, override_settings, Client
+from django.urls import reverse
+from bangtsam.tests.utils import tshong_wagtail_homepage
 
 
 @override_settings(DEBUG=False)
@@ -10,3 +13,15 @@ class TestViewHandler(TestCase):
             "頁面可能被刪除、移動，或是網址輸入錯誤。",
             status_code=404
         )
+
+    def test_handler_renders_500_response(self):
+        client = Client(raise_request_exception=False)
+        with patch("bangtsam.models.HomePage.template",
+                   return_value="") as m:
+            homepage = tshong_wagtail_homepage()
+            response = client.get(homepage.url)
+            self.assertContains(
+                response,
+                "不好意思，伺服器在處理您的請求時遇到了非預期的狀況。",
+                status_code=500
+            )

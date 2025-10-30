c4bbc88887af0d5368fc0a73296b58b254fc7642
bunli <bunli@ithuan.tw>
2025-10-30 18:14:55 +0800

    * 決議無法測試Handler500。

--- autuan/bangtsam/tests/views/test_handler.py
+++ autuan/bangtsam/tests/views/test_handler.py
@@ -1,7 +1,4 @@
-from unittest.mock import patch, DEFAULT
-from django.test import TestCase, override_settings, Client
-from django.urls import reverse
-from bangtsam.tests.utils import tshong_wagtail_homepage
+from django.test import TestCase, override_settings
 
 
 @override_settings(DEBUG=False)
@@ -13,15 +10,3 @@ class TestViewHandler(TestCase):
             "頁面可能被刪除、移動，或是網址輸入錯誤。",
             status_code=404
         )
-
-    def test_handler_renders_500_response(self):
-        client = Client(raise_request_exception=False)
-        with patch("bangtsam.models.HomePage.template",
-                   return_value="") as m:
-            homepage = tshong_wagtail_homepage()
-            response = client.get(homepage.url)
-            self.assertContains(
-                response,
-                "不好意思，伺服器在處理您的請求時遇到了非預期的狀況。",
-                status_code=500
-            )

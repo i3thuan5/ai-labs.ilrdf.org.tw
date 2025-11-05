79d0ecd6968eec8b731f010173558dd3f1730d42
bunli <bunli@ithuan.tw>
2025-11-05 16:58:26 +0800

    * 初步設定Django settings。

--- autuan/autuan/settings-tsiunnsuann.py
+++ autuan/autuan/settings-tsiunnsuann.py
@@ -9,7 +9,38 @@ VIRTUAL_HOST = os.getenv('VIRTUAL_HOST').split(',')
 
 ALLOWED_HOSTS = VIRTUAL_HOST
 
+# Static Files
+
 STATIC_ROOT = '/staticfiles/'
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mypostgres',
+        "USER": "mydatabaseuser",
+        "PASSWORD": "3a2@)5b=4a9b*C28^dD9FG",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
+
+# Security
+
+SECURE_HSTS_SECONDS = 3600
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_AGE =
+CSRF_COOKIE_SAMESITE = "Strict"
+CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SECURE = True
+# SECURE_PROXY_SSL_HEADER = # 文件指出愛符合三要件才設定這
+SECURE_SSL_REDIRECT = True
+# SECURE_SSL_HOST
+SESSION_COOKIE_SAMESITE = "Strict"
+SESSION_COOKIE_SECURE = True
 
 # Wagtail
 

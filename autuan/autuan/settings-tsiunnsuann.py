908c4b1282c0a4a2f073f05bbecfe92b9e439f04
bunli <bunli@ithuan.tw>
2025-11-06 09:06:20 +0800

    * 按Django settings放初步判斷需要的設定檔參數。

--- autuan/autuan/settings-tsiunnsuann.py
+++ autuan/autuan/settings-tsiunnsuann.py
@@ -1,6 +1,8 @@
 import os
 from .settings import *  # noqa
 
+# Core
+
 DEBUG = False
 
 SECRET_KEY = os.getenv('SECRET_KEY')
@@ -19,7 +21,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mypostgres',
-        "USER": "mydatabaseuser",
+        "USER": "mypgsqluser",
         "PASSWORD": "3a2@)5b=4a9b*C28^dD9FG",
         "HOST": "127.0.0.1",
         "PORT": "5432",
@@ -31,17 +33,32 @@ DATABASES = {
 SECURE_HSTS_SECONDS = 3600
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-# CSRF_COOKIE_AGE =
 CSRF_COOKIE_SAMESITE = "Strict"
 CSRF_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True
-# SECURE_PROXY_SSL_HEADER = # 文件指出愛符合三要件才設定這
 SECURE_SSL_REDIRECT = True
-# SECURE_SSL_HOST
 SESSION_COOKIE_SAMESITE = "Strict"
 SESSION_COOKIE_SECURE = True
 
 # Wagtail
 
 WAGTAILADMIN_BASE_URL = VIRTUAL_HOST[0]
+
+
+# 教典有阿莉頂懸無加的
+# CSRF_TRUSTED_ORIGINS = []
+# LANGUAGE_COOKIE_SECURE = True
+# SESSION_COOKIE_HTTPONLY = True
+# SECURE_PROXY_SSL_HEADER = # 文件指出愛符合三要件才設定這
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
+# X_FRAME_OPTIONS = 'DENY'
+# EMAIL_*
+# ADMINS
+# LOGGING
+# SECURE_HSTS_SECONDS = 10
+
+# 愛問的：
+# CSRF_COOKIE_AGE
+# SECURE_SSL_HOST
+# FILE_UPLOAD_MAX_MEMORY_SIZE

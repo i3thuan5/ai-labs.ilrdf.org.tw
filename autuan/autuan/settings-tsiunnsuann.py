d4c6eebe8ea985df8a615962eefcbb3242df5aec
bunli <bunli@ithuan.tw>
2025-11-06 15:34:52 +0800

    * 上線核心設定core settings減少不必要的空行。

--- autuan/autuan/settings-tsiunnsuann.py
+++ autuan/autuan/settings-tsiunnsuann.py
@@ -4,11 +4,8 @@ from .settings import *  # noqa
 # Core
 
 DEBUG = False
-
 SECRET_KEY = os.getenv('SECRET_KEY')
-
 VIRTUAL_HOST = os.getenv('VIRTUAL_HOST').split(',')
-
 ALLOWED_HOSTS = VIRTUAL_HOST
 
 # Static Files
